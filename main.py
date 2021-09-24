ecfe673eb90cb082ceee6b0ac1f2f7f9ca7806c1
nidzh <ragimov93@gmail.com>
2021-09-24 04:49:43 +0300

    my commit

--- main.py
+++ main.py
@@ -15,36 +15,61 @@ async def my_event_handler(event):
     raw_text = event.raw_text.lower()
     text = re.sub('\W', ' ', event.raw_text.lower())
 
-    if raw_text.startwith('/all'):
+    if raw_text == '/help':
+        await client.send_message('+79067836944', f'Доступные команды:\n\n'
+                                                  f'**/help** - `Подсказка`\n\n'
+                                                  f'**/all** - `Показать все активные ключевые слова`\n\n'
+                                                  f'**/add keyword1, keyword2, key word3** - `Добавить ключевые слова в списке. Через запятую, в любом регистре`\n\n'
+                                                  f'**/del keyword1, keyword2, key word3** - `Удалить ключевые слова в списке. Через запятую, в любом регистре`')
+
+        await client.send_message('+79104778970', f'Доступные команды:\n\n'
+                                                  f'**/help** - `Подсказка`\n\n'
+                                                  f'**/all** - `Показать все активные ключевые слова`\n\n'
+                                                  f'**/add keyword1, keyword2, key word3** - `Добавить ключевые слова в списке. Через запятую, в любом регистре`\n\n'
+                                                  f'**/del keyword1, keyword2, key word3** - `Удалить ключевые слова в списке. Через запятую, в любом регистре`')
+
+
+    if raw_text == '/all':
         await client.send_message('+79067836944', f'Использумые ключевые слова: {keywords}')
         await client.send_message('+79104778970', f'Использумые ключевые слова: {keywords}')
 
 
-    elif raw_text.startwith('/add'):
-        add_keywords = event.raw_text.lower().replace('/all ', '').split(',')
-        for keyword in add_keywords:
-            keywords.append(keyword.strip())
-        await client.send_message('+79067836944',
-                                  f'Ключевые слова ДОБАВЛЕНЫ. Все использумые ключевые слова: {keywords}')
-        await client.send_message('+79104778970',
-                                  f'Ключевые слова ДОБАВЛЕНЫ. Все использумые ключевые слова: {keywords}')
-
-    elif raw_text.startwith('/del'):
-        del_keywords = event.raw_text.lower().replace('/del ', '').split(',')
-        for keyword in del_keywords:
-            print(keyword)
-            if keyword.strip() in keywords:
-                keywords.remove(keyword.strip())
+    elif raw_text.startswith('/add'):
+        add_keywords = event.raw_text.lower().replace('/add', '').split(',')
+        if add_keywords[0]:
+            for keyword in add_keywords:
+                keywords.append(keyword.strip())
                 await client.send_message('+79067836944',
-                                          f'Ключевые слова УДАЛЕНЫ. Все использумые ключевые слова: {keywords}')
+                                      f'Ключевое слово {keyword.strip()} ДОБАВЛЕНО. Все использумые ключевые слова: {keywords}')
                 await client.send_message('+79104778970',
-                                          f'Ключевые слова УДАЛЕНЫ. Все использумые ключевые слова: {keywords}')
-            else:
-                await client.send_message('+79067836944',
-                                          f'Ключевое слово {keyword} нет в списке')
-                await client.send_message('+79104778970',
-                                          f'Ключевое слово {keyword} нет в списке')
+                                      f'Ключевое слово {keyword.strip()} ДОБАВЛЕНО. Все использумые ключевые слова: {keywords}')
+        else:
+            await client.send_message('+79067836944',
+                                      f'Попробуйте ещё раз, но с ключевым словом.')
+            await client.send_message('+79104778970',
+                                      f'Попробуйте ещё раз, но с ключевым словом.')
 
+    elif raw_text.startswith('/del'):
+        del_keywords = event.raw_text.lower().replace('/del', '').split(',')
+        if del_keywords[0]:
+            for keyword in del_keywords:
+                print(keyword)
+                if keyword.strip() in keywords:
+                    keywords.remove(keyword.strip())
+                    await client.send_message('+79067836944',
+                                              f'Ключевое слово {keyword.strip()} УДАЛЕНО. Все использумые ключевые слова: {keywords}')
+                    await client.send_message('+79104778970',
+                                              f'Ключевое слово {keyword.strip()} УДАЛЕНО. Все использумые ключевые слова: {keywords}')
+                else:
+                    await client.send_message('+79067836944',
+                                              f'Ключевого слова {keyword.strip()} нет в списке')
+                    await client.send_message('+79104778970',
+                                              f'Ключевого слова {keyword.strip()} нет в списке')
+        else:
+            await client.send_message('+79067836944',
+                                      f'Попробуйте ещё раз, но с ключевым словом.')
+            await client.send_message('+79104778970',
+                                      f'Попробуйте ещё раз, но с ключевым словом.')
 
     else:
         for keyword in keywords:
